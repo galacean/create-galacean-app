579cccfc2e35c10aac0d6742ae3f03fe037d5a71
gz65555 <gz65555@gmail.com>
2021-02-07 16:10:25 +0800

    feat: add create-app

--- src/create-app.ts
+++ src/create-app.ts
@@ -1,12 +1,31 @@
 import fs from "fs";
 import path from "path";
 
-const templatesDir = path.join("../", "templates");
+const templatesDir = path.join(__dirname, "../", "templates");
 
-export function createApp(template: string, cwd: string) {
+export function createApp(template: string, targetPath: string) {
 	const templateDir = path.join(templatesDir, template);
 	const isTemplateExist = fs.existsSync(templateDir);
 	if (!isTemplateExist) {
 		throw `Can't find template: ${template}`;
-  }
+	}
+	copy(templateDir, targetPath);
+}
+
+function copy(src: string, dest: string) {
+	const stat = fs.statSync(src);
+	if (stat.isDirectory()) {
+		copyDir(src, dest);
+	} else {
+		fs.copyFileSync(src, dest);
+	}
+}
+
+function copyDir(srcDir: string, destDir: string) {
+	fs.mkdirSync(destDir, { recursive: true });
+	for (const file of fs.readdirSync(srcDir)) {
+		const srcFile = path.resolve(srcDir, file);
+		const destFile = path.resolve(destDir, file);
+		copy(srcFile, destFile);
+	}
 }
