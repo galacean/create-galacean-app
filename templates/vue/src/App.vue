978968be6a979ec7d54ce6238207900839fefa2b
gz65555 <gz65555@gmail.com>
2021-02-07 16:38:19 +0800

    feat: add vue template

---
+++ templates/vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<canvas style="width: 100vw; height: 100vh" id="canvas" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { createOasis } from "./oasis";
+
+export default defineComponent({
+	name: "App",
+	components: {},
+	mounted: () => {
+		createOasis();
+	},
+});
+</script>
