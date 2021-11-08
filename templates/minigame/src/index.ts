35ea25b2009e59b59b5c76bccf5a948e0d4d15fa
gz65555 <kevin.hs@antgroup.com>
2021-11-08 14:54:22 +0800

    feat: use box template

--- templates/minigame/src/index.ts
+++ templates/minigame/src/index.ts
@@ -1,123 +1,39 @@
-import { OrbitControl } from "@oasis-engine/controls";
 import {
-  AssetType,
-  BlinnPhongMaterial,
-  Camera,
-  Color,
-  DirectLight,
-  Entity,
-  Material,
-  MeshRenderer,
-  ModelMesh,
-  PrimitiveMesh,
-  RenderFace,
-  Script,
-  Texture2D,
-  Vector3,
-  WebGLEngine
+	BlinnPhongMaterial,
+	Camera,
+	MeshRenderer,
+	PrimitiveMesh,
+	Vector3,
+	WebGLEngine,
 } from "oasis-engine";
 
-init();
+const engine = new WebGLEngine("canvas");
+engine.canvas.resizeByClientSize();
+const scene = engine.sceneManager.activeScene;
+const rootEntity = scene.createRootEntity();
 
-function init(): void {
-  // Create engine
-  const engine = new WebGLEngine("canvas");
-  engine.canvas.resizeByClientSize();
+// init camera
+const cameraEntity = rootEntity.createChild("camera");
+cameraEntity.addComponent(Camera);
+const pos = cameraEntity.transform.position;
+pos.setValue(10, 10, 10);
+cameraEntity.transform.position = pos;
+cameraEntity.transform.lookAt(new Vector3(0, 0, 0));
 
-  // Create root entity
-  const scene = engine.sceneManager.activeScene;
-  const rootEntity = scene.createRootEntity();
-  scene.ambientLight.diffuseSolidColor = new Color(0.6, 0.6, 0.6);
+// init light
+scene.ambientLight.diffuseSolidColor.setValue(1, 1, 1, 1);
+scene.ambientLight.diffuseIntensity = 1.2;
 
-  // Create camera
-  const cameraEntity = rootEntity.createChild("Camera");
-  cameraEntity.transform.setPosition(0, 0, 20);
-  cameraEntity.addComponent(Camera);
-  cameraEntity.addComponent(OrbitControl);
+// init cube
+const cubeEntity = rootEntity.createChild("cube");
+const renderer = cubeEntity.addComponent(MeshRenderer);
+const mtl = new BlinnPhongMaterial(engine);
+const color = mtl.baseColor;
+color.r = 0.0;
+color.g = 0.8;
+color.b = 0.5;
+color.a = 1.0;
+renderer.mesh = PrimitiveMesh.createCuboid(engine);
+renderer.setMaterial(mtl);
 
-  // Create direct light
-  const lightEntity = rootEntity.createChild("DirectLight");
-  const light = lightEntity.addComponent(DirectLight);
-  light.intensity = 0.6;
-
-  engine.resourceManager
-    .load<Texture2D>({
-      url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*ArCHTbfVPXUAAAAAAAAAAAAAARQnAQ",
-      type: AssetType.Texture2D
-    })
-    .then((texture) => {
-      const distanceX = 2.5;
-      const distanceY = 2.5;
-      const position = new Vector3();
-
-      // Create material
-      const material = new BlinnPhongMaterial(engine);
-      material.renderFace = RenderFace.Double;
-      material.baseTexture = texture;
-
-      for (let i = 0; i < 3; i++) {
-        const posX = (i - 1) * distanceX;
-
-        // Create cuboid
-        position.setValue(posX, distanceY * 2.5, 0);
-        generatePrimitiveEntity(rootEntity, "cuboid", position, material, PrimitiveMesh.createCuboid(engine));
-
-        // Create sphere
-        position.setValue(posX, distanceY * 1.5, 0);
-        generatePrimitiveEntity(rootEntity, "sphere", position, material, PrimitiveMesh.createSphere(engine));
-
-        // Create plane
-        position.setValue(posX, distanceY * 0.5, 0);
-        generatePrimitiveEntity(rootEntity, "plane", position, material, PrimitiveMesh.createPlane(engine));
-
-        // Create cylinder
-        position.setValue(posX, -distanceY * 0.5, 0);
-        generatePrimitiveEntity(rootEntity, "cylinder", position, material, PrimitiveMesh.createCylinder(engine));
-
-        // Create cone
-        position.setValue(posX, -distanceY * 1.5, 0);
-        generatePrimitiveEntity(rootEntity, "cone", position, material, PrimitiveMesh.createCone(engine));
-
-        // Create turos
-        position.setValue(posX, -distanceY * 2.5, 0);
-        generatePrimitiveEntity(rootEntity, "torus", position, material, PrimitiveMesh.createTorus(engine));
-      }
-    });
-
-  // Run engine
-  engine.run();
-}
-
-/**
- * generate primitive mesh entity.
- */
-function generatePrimitiveEntity(
-  rootEntity: Entity,
-  name: string,
-  position: Vector3,
-  material: Material,
-  mesh: ModelMesh
-): Entity {
-  const entity = rootEntity.createChild(name);
-  entity.transform.setPosition(position.x, position.y, position.z);
-  entity.addComponent(RotateScript);
-  const renderer = entity.addComponent(MeshRenderer);
-  renderer.mesh = mesh;
-  renderer.setMaterial(material);
-
-  return entity;
-}
-
-/**
- * Script for rotate.
- */
-class RotateScript extends Script {
-  /**
-   * @override
-   * The main loop, called frame by frame.
-   * @param deltaTime - The deltaTime when the script update.
-   */
-  onUpdate(deltaTime: number): void {
-    this.entity.transform.rotate(0.5, 0.6, 0);
-  }
-}
+engine.run();
