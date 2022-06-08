ce0651667cd710250dd0f4208fec63422118e73e
joypeng <70848683+FliPPeDround@users.noreply.github.com>
2022-06-08 11:07:26 +0800

    feat(templates/vue): use vue3 compositon API (#6)

--- templates/vue/src/App.vue
+++ templates/vue/src/App.vue
@@ -1,16 +1,13 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { createOasis } from "./oasis";
+
+onMounted(() => {
+  createOasis();
+})
+</script>
+
 <template>
 	<canvas style="width: 100vw; height: 100vh" id="canvas" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import { createOasis } from "./oasis";
-
-export default defineComponent({
-	name: "App",
-	components: {},
-	mounted: () => {
-		createOasis();
-	},
-});
-</script>
