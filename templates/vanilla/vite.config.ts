6dc9f465f4394f04cd223995b25562705734c041
gz65555 <gz65555@gmail.com>
2023-12-29 14:54:39 +0800

    update vite config

--- templates/vanilla/vite.config.ts
+++ templates/vanilla/vite.config.ts
@@ -1,9 +1,7 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
   resolve: {
     dedupe: ["@galacean/engine"],
   },
